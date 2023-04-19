3f019e1a16c4cab890086e3f0fe9b386e6ee471c
Tatsiana Kulikouskaya <tatsiana@pedanov.ru>
2023-04-19 16:15:35 +0300

    Resolved few more tasks

--- index.js
+++ index.js
@@ -247,3 +247,66 @@ function sortEmotions(arr, order) {
 	}
 	return arr;
 }
+
+/* Someone was hacking the score. Each student's record is given as an array.
+The objects in the array are given again as an array of scores for each name and total score.
+The scores for each grade is:
+A: 30 points
+B: 20 points
+C: 10 points
+D: 5 points
+Everything else: 0 points
+If there are 5 or more courses and all courses has a grade of B or above, additional 20 points are awarded.
+After all the calculations, the total score should be capped at 200 points.
+Returns the name of the hacked name as an array when scoring with this rule. */
+
+function findHack(arr) {
+	const result = [];
+	const glossary = new Map ([
+	  ['A', 30],
+	  ['B', 20], 
+	  ['C', 10],
+	  ['D', 5]
+	]);
+	
+	arr.forEach(([name, total, scores]) => {
+	  const points = scores.map((score) => {
+		return glossary.has(score) ? glossary.get(score) : 0;
+	  })
+	  
+	 if (points.length >= 5 && points.every(p => p >= 20)) {
+		points.push(20)
+	  }
+	 
+	  const sum = points.reduce((sum, point) => sum + point, 0);
+	 
+	  if (sum !== total || sum > 200) {
+		result.push(name);
+	  }
+	})
+	
+	return result;
+}
+
+/* Your job here is to write a function, which takes a sorted array ary and a value val,
+and returns the lowest index where you could insert val to maintain the sorted-ness of the array.
+The input array will always be sorted in ascending order. It may contain duplicates. 
+Do not modify the input. */
+
+function keepOrder(ary, val) {
+	return ary.filter(num => num < val).length;
+}
+
+function keepOrder(ary, val) {
+
+	if (ary[0] >= val) return 0;
+
+	if (ary[ary.length - 1] < val) return ary.length;
+
+	for (let i = 0; i < ary.length; i++) {
+		if (ary[i-1] < val && ary[i] >= val) { 
+			return i
+		}
+	}
+}
+
